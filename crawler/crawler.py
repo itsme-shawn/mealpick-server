044a6791db44fee04f1a1be5ac1c865a846bfe9e
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-12-19 08:08:13 +0900

    [feat]: selenium headless 에 user-agent 옵션 추가

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -16,6 +16,11 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("window-size=1920x1080")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+)
+options.add_argument("lang=ko_KR")
 
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=options
